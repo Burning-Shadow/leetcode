bbabcc5db47b84d89692d2e81d86a8fbb92c7e9c
francisswang <2485999859@qq.com>
2022-08-04 21:30:44 +0800

    1356

--- sort-integers-by-the-number-of-1-bits[1356].js
+++ sort-integers-by-the-number-of-1-bits[1356].js
@@ -18,3 +18,17 @@ var sortByBits = function (arr) {
   // 如果有差，则按 bits 数排，如果无差，则按原值排
   return arr.sort((a, b) => bitCount(a) - bitCount(b) || a - b);
 };
+
+var sortByBits = function (arr) {
+  let hander = function (str) { return str.toString(2).split('').filter(x => { return x == 1 }).length }
+  arr.sort((a, b) => { return a - b })
+  return arr.sort((a, b) => {
+    return hander(a) - hander(b)
+  })
+};
+
+console.log(sortByBits([0, 1, 2, 3, 4, 5, 6, 7, 8])); // [0,1,2,4,8,3,5,6,7]
+console.log(sortByBits([1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1])); // [1,2,4,8,16,32,64,128,256,512,1024]
+console.log(sortByBits([10000, 10000])); // [10000,10000]
+console.log(sortByBits([2, 3, 5, 7, 11, 13, 17, 19])); // [2,3,5,17,7,11,13,19]
+console.log(sortByBits([10, 100, 1000, 10000])); // [10,100,10000,1000]
