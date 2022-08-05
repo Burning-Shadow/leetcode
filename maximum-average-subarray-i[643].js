92c0848f3a80123ca1208a269bf0a0eea9c970fc
francisswang <2485999859@qq.com>
2022-08-05 16:28:15 +0800

    643

---
+++ maximum-average-subarray-i[643].js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function (nums, k) {
+  let sum = 0;
+  const n = nums.length;
+  for (let i = 0; i < k; i++) sum += nums[i];
+  let maxSum = sum;
+  for (let i = k; i < n; i++) {
+    sum = sum - nums[i - k] + nums[i];
+    maxSum = Math.max(maxSum, sum);
+  }
+  return maxSum / k;
+};
+
+console.log(findMaxAverage([1, 12, -5, -6, 50, 3], 4)); // 12.75
+console.log(findMaxAverage([5], 1)); // 5
