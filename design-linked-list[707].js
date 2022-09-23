5442aa85d303651b68528547bb2758fb3c0fb231
francisswang <2485999859@qq.com>
2022-09-23 11:38:35 +0800

    707

--- design-linked-list[707].js
+++ design-linked-list[707].js
@@ -3,6 +3,10 @@ var MyLinkedList = function () {
   this.head = new ListNode(0);
 };
 
+/** 
+ * @param {number} index
+ * @return {number}
+ */
 MyLinkedList.prototype.get = function (index) {
   if (index < 0 || index >= this.size) {
     return -1;
@@ -14,14 +18,27 @@ MyLinkedList.prototype.get = function (index) {
   return cur.val;
 };
 
+/** 
+ * @param {number} val
+ * @return {void}
+ */
 MyLinkedList.prototype.addAtHead = function (val) {
   this.addAtIndex(0, val);
 };
 
+/** 
+ * @param {number} val
+ * @return {void}
+ */
 MyLinkedList.prototype.addAtTail = function (val) {
   this.addAtIndex(this.size, val);
 };
 
+/** 
+ * @param {number} index 
+ * @param {number} val
+ * @return {void}
+ */
 MyLinkedList.prototype.addAtIndex = function (index, val) {
   if (index > this.size) {
     return;
@@ -37,6 +54,10 @@ MyLinkedList.prototype.addAtIndex = function (index, val) {
   pred.next = toAdd;
 };
 
+/** 
+ * @param {number} index
+ * @return {void}
+ */
 MyLinkedList.prototype.deleteAtIndex = function (index) {
   if (index < 0 || index >= this.size) {
     return;
@@ -53,3 +74,13 @@ function ListNode(val, next) {
   this.val = (val === undefined ? 0 : val)
   this.next = (next === undefined ? null : next)
 }
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
