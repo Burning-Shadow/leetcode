090bf3571cc8a803d9a629bc65cc9fa4439ced97
francisswang <2485999859@qq.com>
2022-11-02 10:48:33 +0800

    54

--- spiral-matrix[54].js
+++ spiral-matrix[54].js
@@ -3,13 +3,6 @@
  * @return {number[]}
  */
 var spiralOrder = function (matrix) {
-  const m = matrix.length;
-  const n = matrix[0].length;
-
-
-};
-
-var spiralOrder = function (matrix) {
   if (!matrix.length || !matrix[0].length) {
     return [];
   }
@@ -32,6 +25,41 @@ var spiralOrder = function (matrix) {
   }
   return order;
 };
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ * 
+ * 1. 首先设定上下左右边界
+ * 2. 其次向右移动到最右，此时第一行因为已经使用过了，可以将其从图中删去，体现在代码中就是重新定义上边界
+ * 3. 判断若重新定义后，上下边界交错，表明螺旋矩阵遍历结束，跳出循环，返回答案
+ * 4. 若上下边界不交错，则遍历还未结束，接着向下向左向上移动，操作过程与第一，二步同理
+ * 5. 不断循环以上步骤，直到某两条边界交错，跳出循环，返回答案
+ */
+var spiralOrder = function (matrix) {
+  if (!matrix.length || !matrix[0].length) return [];
+  const result = [];
+
+  let up = 0,
+    down = matrix.length - 1,
+    left = 0,
+    right = matrix[0].length - 1;
+
+  while (true) {
+    for (let i = left; i <= right; ++i) result.push(matrix[up][i]);
+    if (++up > down) break;
+    for (let i = up; i <= down; ++i) result.push(matrix[i][right]); //向下
+    if (--right < left) break; //重新设定有边界
+    for (let i = right; i >= left; --i) result.push(matrix[down][i]); //向左
+    if (--down < up) break; //重新设定下边界
+    for (let i = down; i >= up; --i) result.push(matrix[i][left]); //向上
+    if (++left > right) break; //重新设定左边界
+  }
+
+  return result;
+};
+
+
 
 
 console.log(spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]])); // [1,2,3,6,9,8,7,4,5]
