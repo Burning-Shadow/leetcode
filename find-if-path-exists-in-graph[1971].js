6807cacc6d1d0a6b62a92d63d6c0ba671808d85a
francisswang <2485999859@qq.com>
2022-12-19 10:21:46 +0800

    1971

--- find-if-path-exists-in-graph[1971].js
+++ find-if-path-exists-in-graph[1971].js
@@ -57,6 +57,43 @@ var validPath = function (n, edges, source, destination) {
 	return dfs(path, source);
 };
 
+
+
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ * 
+ * bfs
+ */
+var validPath = function (n, edges, source, destination) {
+	const adj = new Array(n).fill(0).map(() => new Array());
+	for (const edge of edges) {
+		const x = edge[0], y = edge[1];
+		adj[x].push(y);
+		adj[y].push(x);
+	}
+	const visited = new Array(n).fill(false);
+	const queue = [source];
+	visited[source] = true;
+	while (queue.length) {
+		const vertex = queue.shift();
+		if (vertex === destination) {
+			break;
+		}
+		for (const next of adj[vertex]) {
+			if (!visited[next]) {
+				queue.push(next);
+				visited[next] = true;
+			}
+		}
+	}
+	return visited[destination];
+};
+
+
 console.log(validPath(3, [[0, 1], [1, 2], [2, 0]], 0, 2)); // true
 console.log(validPath(6, [[0, 1], [0, 2], [3, 5], [5, 4], [4, 3]], 0, 5)); // false
 console.log(validPath(10, [[4, 3], [1, 4], [4, 8], [1, 7], [6, 4], [4, 2], [7, 4], [4, 0], [0, 9], [5, 4]], 5, 9)); // true
