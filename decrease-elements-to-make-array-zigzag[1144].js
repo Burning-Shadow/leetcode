7af93939059da06586ff0fe7abbd0b061b5612d0
francisswang <2485999859@qq.com>
2023-02-27 22:22:25 +0800

    1144

---
+++ decrease-elements-to-make-array-zigzag[1144].js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var movesToMakeZigzag = function (nums) {
+  return Math.min(help(nums, 0), help(nums, 1));
+}
+
+const help = (nums, pos) => {
+  let result = 0;
+  for (let i = pos; i < nums.length; i += 2) {
+    let a = 0;
+    if (i - 1 >= 0) {
+      a = Math.max(a, nums[i] - nums[i - 1] + 1);
+    }
+    if (i + 1 < nums.length) {
+      a = Math.max(a, nums[i] - nums[i + 1] + 1);
+    }
+    result += a;
+  }
+  return result;
+};
+
+
+
+
+
+console.log(movesToMakeZigzag()); // 
