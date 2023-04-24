8671591124e499281c1c6f2d72099e6b204ce966
francisswang <2485999859@qq.com>
2023-04-24 22:05:24 +0800

    1163

---
+++ last-substring-in-lexicographical-order[1163].js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var lastSubstring = function (s) {
+  let i = 0, j = 1, n = s.length;
+  while (j < n) {
+    let k = 0;
+    while (j + k < n && s[i + k] === s[j + k]) {
+      k++;
+    }
+    if (j + k < n && s[i + k] < s[j + k]) {
+      let t = i;
+      i = j;
+      j = Math.max(j + 1, t + k + 1);
+    } else {
+      j = j + k + 1;
+    }
+  }
+  return s.substring(i, n);
+};
+
+
+
+
+console.log(lastSubstring("abab")); // "bab"
+console.log(lastSubstring("leetcode")); // "tcode"
